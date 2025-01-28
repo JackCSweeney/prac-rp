2a7d853343f928a699456f17ae9520c4d5109b43
Jack Sweeney <jackcsweeney@gmail.com>
2025-01-28 10:13:03 -0800

    feat: Implement TextChanger component for titles on landing page of application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import ContactModal from './components/ContactModal'
-import React, {useState} from 'react';
+import React, {useState} from 'react'
+import TextChanger from './components/TextChanger'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const titles = ['Software Engineer', 'Enthusiastic Communicator', 'Chef', 'Music Nerd']
 
   return (
     <div>
-      <h1>Jack Sweeney - Software Engineer</h1>
+      <h1>Jack Sweeney<TextChanger texts={titles} interval={2000}/></h1>
       <button onClick={() => setIsOpen(true)}>Contact Information</button>
       {isOpen && <ContactModal setIsOpen={setIsOpen}/>}
     </div>
