8bb3a716de8a85fd67a01f8b68980371de0d4948
Jack Sweeney <jackcsweeney@gmail.com>
2025-01-28 13:46:04 -0800

    feat: Add Projects and ProjectCard components to display coding project information on dashboard

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "../App.css"
+import { Card } from "@mui/material"
+
+
+const ProjectCard = ({ project }) => {
+  return (
+    <div>
+    <Card sx={{ minWidth: 275, maxWidth: 500, color: 'black', bgcolor: 'lightgray', borderColor: 'black' }}>
+      <div>
+        <div>
+          <img src={project.imagePath} className='header-image'/>
+        </div>
+      </div>
+      <h1>{project.title}</h1>
+      <h3>Tech Stack: {project.techStack}</h3>
+      <p>{project.description}</p>
+      <a href={project.githubLink}>GitHub Repository</a>
+    </Card>
+    </div>
+  )
+}
+export default ProjectCard
